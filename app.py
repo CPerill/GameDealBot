afab042c1e32567df018748b9d029cbaa79efae0
connor-jos-perill <connor.jos.perill@hpe.com>
2017-05-11 14:53:40 +0100

    Added Database
    Hoping this will work now haha

--- app.py
+++ app.py
@@ -5,8 +5,26 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import praw
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    import yourapplication.models
+    Base.metadata.create_all(bind=engine)
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 reddit = praw.Reddit(client_id='taGkw_H9ngsFzQ',
                      client_secret='jvuzMogt35XJTxrmf3E1hX9ZH-M',
@@ -227,4 +245,5 @@ class Posts(db.Model):
 
 
 if __name__ == '__main__':
+    print 'Starting app'
     app.run()
---
+++ config.py
@@ -0,0 +1,16 @@
+"""
+Author:     Connor Perill
+Program:    Hello - Testing that Flask is working correctly
+Usage:      For parsing what the user wants
+                Functions:
+                    parsetext
+
+
+"""
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite' + os.path.join(basedir, 'app.sqlite')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repositry')

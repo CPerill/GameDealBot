99af1104ef21e0f25a7db54252562c6d67c149b7
connor-jos-perill <connor.jos.perill@hpe.com>
2017-05-11 15:04:33 +0100

    Pushed incorrect app correction
    Opps

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite' + os.path.join(basedir, 'app.sqlite')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repositry')

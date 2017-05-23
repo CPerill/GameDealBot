a25e6dd1a5fce01c1290371e0a19bf3b598b6aad
connor-jos-perill <connor.jos.perill@hpe.com>
2017-05-23 17:01:01 +0100

    Trying different code

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, request
-import json
-import requests
+
 from flask_sqlalchemy import SQLAlchemy
 import os
 import praw
+import json
+import requests
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import scoped_session, sessionmaker
+#from sqlalchemy.ext.declarative import declarative_base
 #engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
 #db_session = scoped_session(sessionmaker(autocommit=False,
                                          #autoflush=False,
                                          #bind=engine))
 #Base = declarative_base()
 #Base.query = db_session.query_property()
-
 #def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
